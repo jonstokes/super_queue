84a65d15174da4dbabe84d843771ce5fd25a9809
Jon Stokes <jon@paperhatpress.com>
2013-02-09 15:06:48 -0800

    Cleaned up test nesting

--- spec/super_queue_spec.rb
+++ spec/super_queue_spec.rb
@@ -13,72 +13,76 @@ describe SuperQueue do
   end
 
   describe "#new" do
-    it "should create a new SuperQueue" do
-      queue = SuperQueue.new(@defaults)
-      queue.should be_an_instance_of SuperQueue
-    end
-
-    it "should create a new SuperQueue with a url" do
-      queue = SuperQueue.new(@defaults)
-      queue.url.should include("http")
-    end
-
-    it "should create a new SuperQueue with the correct name" do
-      queue = SuperQueue.new(@defaults)
-      queue.url.should include(@defaults[:name])
-    end
-
-    it "should create a new localized SuperQueue by default" do
-      queue = SuperQueue.new(@defaults)
-      queue.should be_localized
-    end
-
     it "should require non-nil options" do
       expect {
         SuperQueue.new(nil)
       }.to raise_error(RuntimeError, "Options can't be nil!")
     end
 
-    it "should require an AWS access key ID" do
-      @defaults.delete(:aws_access_key_id)
-      expect {
-        SuperQueue.new(@defaults)
-      }.to raise_error(RuntimeError, "AWS credentials :aws_access_key_id and :aws_secret_access_key required!")
+    describe "with standard defaults" do
+      before :each do
+        @queue = SuperQueue.new(@defaults)
+      end
+
+      it "should create a new SuperQueue" do
+        @queue.should be_an_instance_of SuperQueue
+      end
+
+      it "should create a new SuperQueue with a url" do
+        @queue.url.should include("http")
+      end
+
+      it "should create a new SuperQueue with the correct name" do
+        @queue.url.should include(@defaults[:name])
+      end
+
+      it "should create a new localized SuperQueue by default" do
+        @queue.should be_localized
+      end
     end
 
-    it "should require an AWS secret" do
-      @defaults.delete(:aws_secret_access_key)
-      expect {
-        SuperQueue.new(@defaults)
-      }.to raise_error(RuntimeError, "AWS credentials :aws_access_key_id and :aws_secret_access_key required!")
-    end
+    describe "with missing or incorrect defaults" do
+      it "should require an AWS access key ID" do
+        @defaults.delete(:aws_access_key_id)
+        expect {
+          SuperQueue.new(@defaults)
+        }.to raise_error(RuntimeError, "AWS credentials :aws_access_key_id and :aws_secret_access_key required!")
+      end
 
-    it "should require a buffer size" do
-      @defaults.delete(:buffer_size)
-      expect {
-        SuperQueue.new(@defaults)
-      }.to raise_error(RuntimeError, "Parameter :buffer_size required!")
-    end
+      it "should require an AWS secret" do
+        @defaults.delete(:aws_secret_access_key)
+        expect {
+          SuperQueue.new(@defaults)
+        }.to raise_error(RuntimeError, "AWS credentials :aws_access_key_id and :aws_secret_access_key required!")
+      end
 
-    it "should require a minimum buffer size" do
-      @defaults.merge!(:buffer_size => 0)
-      expect {
-        SuperQueue.new(@defaults)
-      }.to raise_error(RuntimeError, "Minimun :buffer_size is 5.")
-    end
+      it "should require a buffer size" do
+        @defaults.delete(:buffer_size)
+        expect {
+          SuperQueue.new(@defaults)
+        }.to raise_error(RuntimeError, "Parameter :buffer_size required!")
+      end
 
-    it "should require a queue name" do
-      @defaults.delete(:name)
-      expect {
-        SuperQueue.new(@defaults)
-      }.to raise_error(RuntimeError, "Parameter :name required!")
-    end
+      it "should require a minimum buffer size" do
+        @defaults.merge!(:buffer_size => 0)
+        expect {
+          SuperQueue.new(@defaults)
+        }.to raise_error(RuntimeError, "Minimun :buffer_size is 5.")
+      end
 
-    it "should require that visibility timeout be an integer" do
-      @defaults.merge!(:visibility_timeout => "a")
-      expect {
-        SuperQueue.new(@defaults)
-      }.to raise_error(RuntimeError, "Visbility timeout must be an integer (in seconds)!")
+      it "should require a queue name" do
+        @defaults.delete(:name)
+        expect {
+          SuperQueue.new(@defaults)
+        }.to raise_error(RuntimeError, "Parameter :name required!")
+      end
+
+      it "should require that visibility timeout be an integer" do
+        @defaults.merge!(:visibility_timeout => "a")
+        expect {
+          SuperQueue.new(@defaults)
+        }.to raise_error(RuntimeError, "Visbility timeout must be an integer (in seconds)!")
+      end
     end
   end
 
