65c61f13609167409b4d75890bc454bfe58e9c4b
Jon Stokes <jon@paperhatpress.com>
2013-02-09 14:43:08 -0800

    More working tests for new

--- spec/super_queue_spec.rb
+++ spec/super_queue_spec.rb
@@ -4,7 +4,7 @@ describe SuperQueue do
 
   before :each do
     Fog.mock!
-    @default_required_options = {
+    @defaults = {
       :aws_access_key_id => "abc123",
       :aws_secret_access_key => "123abc",
       :name => "rspec-test",
@@ -14,15 +14,28 @@ describe SuperQueue do
 
   describe "#new" do
     it "should create a new SuperQueue" do
-      queue = SuperQueue.new(@default_required_options)
+      queue = SuperQueue.new(@defaults)
       queue.should be_an_instance_of SuperQueue
     end
   end
 
-  it "should require an AWS access key ID" do
-    @default_required_options.delete(:aws_access_key_id)
+  it "should require non-nil options" do
     expect {
-      SuperQueue.new(@default_required_options)
+      SuperQueue.new(nil)
+    }.to raise_error(RuntimeError, "Options can't be nil!")
+  end
+
+  it "should require an AWS access key ID" do
+    @defaults.delete(:aws_access_key_id)
+    expect {
+      SuperQueue.new(@defaults)
+    }.to raise_error("AWS credentials :aws_access_key_id and :aws_secret_access_key required!")
+  end
+
+  it "should require an AWS secret" do
+    @defaults.delete(:aws_secret_access_key)
+    expect {
+      SuperQueue.new(@defaults)
     }.to raise_error("AWS credentials :aws_access_key_id and :aws_secret_access_key required!")
   end
 
